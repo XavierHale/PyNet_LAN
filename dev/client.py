96f6572b921f83b3eedfae7cec61a0fc564bae3e
Xavier Hale <81330980+XavierHale@users.noreply.github.com>
2023-05-10 19:35:40 -0400

    Fixing Some things

--- dev/client.py
+++ dev/client.py
@@ -15,25 +15,42 @@ import time
 ver = 1.0
 
 
-
 def err(type, msg):
     print("We encountered an error. Here are the details:")
     print("Type:", type, "Message", msg)
+
+
 s = socket.socket()
 st = 0.02
+
+
 def sp(text):
-  for char in text:
-    print(Fore.LIGHTGREEN_EX + char, end="", flush=True)
-    time.sleep(st)
+    for char in text:
+        print(Fore.LIGHTGREEN_EX + char, end="", flush=True)
+        time.sleep(st)
+
+
 ctypes.windll.kernel32.SetConsoleTitleW("NOCOM")
 # init colors
 init()
 
 # set the available colors
-colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.LIGHTBLACK_EX, 
-    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
-    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX, Fore.LIGHTWHITE_EX, 
-    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED, Fore.WHITE, Fore.YELLOW
+colors = [
+    Fore.BLUE,
+    Fore.CYAN,
+    Fore.GREEN,
+    Fore.LIGHTBLACK_EX,
+    Fore.LIGHTBLUE_EX,
+    Fore.LIGHTCYAN_EX,
+    Fore.LIGHTGREEN_EX,
+    Fore.LIGHTMAGENTA_EX,
+    Fore.LIGHTRED_EX,
+    Fore.LIGHTWHITE_EX,
+    Fore.LIGHTYELLOW_EX,
+    Fore.MAGENTA,
+    Fore.RED,
+    Fore.WHITE,
+    Fore.YELLOW,
 ]
 
 # choose a random color for the client
@@ -51,14 +68,16 @@ sp("Loaded NOCOM from NETWORK_EXPL")
 print("")
 time.sleep(1)
 ctypes.windll.kernel32.SetConsoleTitleW("PyNet Version 1.1")
+
+
 def pubchat():
     # server's IP address
-    # if the server is not on this machine, 
+    # if the server is not on this machine,
     # put the private (network) IP address (e.g 192.168.1.2)
     sp("Enter IP to connect to:")
-    SERVER_HOST= input ("")
-    SERVER_PORT = 5002 # server's port
-    separator_token = "<SEP>" # we will use this to separate the client name & message
+    SERVER_HOST = input("")
+    SERVER_PORT = 5002  # server's port
+    separator_token = "<SEP>"  # we will use this to separate the client name & message
     sp("Hooking into network thread...")
     time.sleep(1)
     print("")
@@ -77,10 +96,12 @@ def pubchat():
 
     sp("You can start chatting. Type q to return to the menu.")
     print("")
+
     def listen_for_messages():
         while True:
             message = s.recv(1024).decode()
             print("\n" + message)
+
     # make a thread that listens for messages to this client & print them
     t = Thread(target=listen_for_messages)
     # make the thread daemon so it ends whenever the main thread ends
@@ -89,43 +110,57 @@ def pubchat():
     t.start()
     while True:
         # input message we want to send to the server
-        to_send =  input(">")
+        to_send = input(">")
         # a way to exit the program
-        if to_send.lower() == 'q':
+        if to_send.lower() == "q":
             break
         # add the datetime, name & the color of the sender
-        date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
-        to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+        date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        to_send = (
+            f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+        )
         # finally, send the message
         s.send(to_send.encode())
-    choice()
+    choice(0)
+
+
 def privchat():
     print("Do you want to host or connect to a private server?")
-    print("""
+    print(
+        """
     (1) Host
     (2) Connect
 
-    """)
+    """
+    )
     privchoice = input(">")
     if privchoice == "1":
         privcode = random.randint(1025, 65536)
-        print(">", privcode, "< This is your private server code. This changes every time you host, so keep that in mind")
-        tempprivip = socket.gethostname() 
+        print(
+            ">",
+            privcode,
+            "< This is your private server code. This changes every time you host, so keep that in mind",
+        )
+        tempprivip = socket.gethostname()
         privip = socket.gethostbyname(tempprivip)
         print(">", privip, "< This is your IP. It does not change every time you host.")
-        print("You will have to give both of these to the people who are connectiing to you.")
+        print(
+            "You will have to give both of these to the people who are connectiing to you."
+        )
         print("Initializing server...")
         privateserver.run(privcode)
     if privchoice == "2":
-            # server's IP address
-        # if the server is not on this machine, 
+        # server's IP address
+        # if the server is not on this machine,
         # put the private (network) IP address (e.g 192.168.1.2)
         print("Enter IP to connect to:")
-        SERVER_HOST= input (">")
+        SERVER_HOST = input(">")
         print("Enter your private server code:")
-        SERVER_PORT = input (">")
+        SERVER_PORT = input(">")
         SERVER_PORT = int(SERVER_PORT)
-        separator_token = "<SEP>" # we will use this to separate the client name & message
+        separator_token = (
+            "<SEP>"  # we will use this to separate the client name & message
+        )
         sp("Hooking into network thread...")
         time.sleep(1)
         print("")
@@ -144,10 +179,12 @@ def privchat():
 
         sp("You can start chatting. Type q to return to the menu.")
         print("")
+
         def listen_for_messages():
             while True:
                 message = s.recv(1024).decode()
                 print("\n" + message)
+
         # make a thread that listens for messages to this client & print them
         t = Thread(target=listen_for_messages)
         # make the thread daemon so it ends whenever the main thread ends
@@ -156,25 +193,30 @@ def privchat():
         t.start()
         while True:
             # input message we want to send to the server
-            to_send =  input(">")
+            to_send = input(">")
             # a way to exit the program
-            if to_send.lower() == 'q':
+            if to_send.lower() == "q":
                 break
             # add the datetime, name & the color of the sender
-            date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+            date_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
             # finally, send the message
             s.send(to_send.encode())
-        choice()
+        choice(0)
+
 
 def gamemenu():
-    print("This is still in progress, crap may just ramdomly crash your computer if it feels like it")
-    print("""
+    print(
+        "This is still in progress, crap may just ramdomly crash your computer if it feels like it"
+    )
+    print(
+        """
     (1) Host a Doom server
     (2) Connect to a Doom server
     (3) Snake?
     (4) Back
-    """)
+    """
+    )
     gamechoice = input("Enter your choice>")
     if gamechoice == "1":
         err("02", "Not implemented")
@@ -186,10 +228,14 @@ def gamemenu():
         os.startfile("notsnake.py")
         gamemenu()
     if gamechoice == "4":
-        choice()
-def choice():
-    os.system('cls')
-    print("""
+        choice(0)
+
+
+def choice(nclr):
+    if nclr != 1 :
+        os.system("cls")
+    print(
+        """
 Welcome to PyNet! This is a local messaging service, sorta like a BBS.
 
 What would you like to do?
@@ -198,12 +244,13 @@ What would you like to do?
 (3) Games          
 (4) About
 
-    """)
+    """
+    )
     boardchoice = input("Enter your choice>")
     if boardchoice == "1":
         print("Why would you choose this? Literaly nothing here.")
 
-        choice()
+        choice(0)
     if boardchoice == "2":
         privchat()
     if boardchoice == "3":
@@ -212,8 +259,10 @@ What would you like to do?
         print("PyNet Version", ver)
         print("Developed by Xavier Hale")
         print("Version complied on 2/16/2023")
-        choice()
-choice()
+        choice(1)
 
-    # close the socket
+
+choice(0)
+
+# close the socket
 s.close()
